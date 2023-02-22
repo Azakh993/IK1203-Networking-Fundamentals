47ed249038106abe971132892a874666c946cd99
Azmeer K <azmeer.khawaja@outlook.com>
2023-02-22 17:26:40 +0100

    Restructured. Checks for HTTP/1.1. Fixed bug which could not handle favicon request.

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -5,10 +5,10 @@ import java.io.*;
 import java.util.HashMap;
 
 public class HTTPAsk {
+    private final static String NEWLINE = "\r\n";
     private static HashMap< String, Object > arguments;
 
     public static void main(String[] args) {
-
         try {
             int server_port = Integer.parseInt(args[0]);
             ServerSocket serverSocket = new ServerSocket(server_port);
@@ -21,20 +21,26 @@ public class HTTPAsk {
                     System.out.println("Connected...");
 
                     String unparsedRequest = read_request(socket);
-                    set_arguments(unparsedRequest);
+                    parse_and_set_parameters(unparsedRequest);
                     System.out.println("Processed request...");
 
-                    get_server_response(socket);
-                    System.out.println("Responded to query...");
+                    OutputStream output = socket.getOutputStream();
+                    boolean parameters_valid = verify_parameters(output);
+
+                    if (parameters_valid) {
+                        byte[] serverResponse = ask_server();
+                        output_response(output, serverResponse);
+                    }
 
                     socket.close();
                     System.out.println("Closed connection!");
-                } catch (IOException exception) {
-                    exception.printStackTrace();
+
+                } catch (Exception exception) {
+                    System.err.println(exception);
                 }
             }
-        } catch (Exception exception) {
-            exception.printStackTrace();
+        } catch (Exception exception){
+            System.err.println(exception);
         }
     }
 
@@ -50,7 +56,7 @@ public class HTTPAsk {
         return requestBuilder.toString();
     }
 
-    private static void set_arguments(String unparsedRequest) {
+    private static void parse_and_set_parameters(String unparsedRequest) {
         String[] requestComponents = unparsedRequest.split("[?&= ]");
         HashMap< String, Object > arguments_map = new HashMap<>();
 
@@ -62,7 +68,12 @@ public class HTTPAsk {
             arguments_map.put("/ask", "VALID");
         }
 
-        for (int i = 2; i < requestComponents.length - 1; i++) {
+        int lastRequestParameterIndex = requestComponents.length - 1;
+        if (requestComponents[lastRequestParameterIndex].equals("HTTP/1.1")) {
+            arguments_map.put("HTTP/1.1", "VALID");
+        }
+
+        for (int i = 2; i < lastRequestParameterIndex; i++) {
             String argumentType = requestComponents[i].trim();
             String argumentValue = requestComponents[++i].trim();
 
@@ -81,58 +92,68 @@ public class HTTPAsk {
         arguments = arguments_map;
     }
 
-    private static void get_server_response(Socket socket) throws IOException {
-        OutputStream output = socket.getOutputStream();
 
-        String hostName = (String) arguments.get("hostname");
-        Integer port = (Integer) arguments.get("port");
-        boolean getFirst = arguments.containsKey("GET") && arguments.get("GET").equals("VALID");
-        boolean askSecond = arguments.containsKey("/ask") && arguments.get("/ask").equals("VALID");
+    private static boolean verify_parameters(OutputStream output) throws IOException {
+        boolean hostProvided = arguments.containsKey("hostname");
+        boolean portProvided = arguments.containsKey("port");
+        boolean getValid = arguments.containsKey("GET") && arguments.get("GET").equals("VALID");
+        boolean askValid = arguments.containsKey("/ask") && arguments.get("/ask").equals("VALID");
+        boolean httpValid = arguments.containsKey("HTTP/1.1") && arguments.get("HTTP/1.1").equals("VALID");
 
-        byte[] HTTP400 = "HTTP/1.1 400 Bad Request\r\n\r\n".getBytes();
+        String HTTP400 = "HTTP/1.1 400 Bad Request" + NEWLINE;
+        String HTTP404 = "HTTP/1.1 404 Not Found" + NEWLINE;
+        String HTTP505 = "HTTP/1.1 505 HTTP Version Not Supported" + NEWLINE;
+        String HTTP200 = "HTTP/1.1 200 OK" + NEWLINE;
+        String connection = "Connection: close" + NEWLINE;
+        String contentType = "Content-Type: text/plain" + NEWLINE;
 
-        if (hostName == null || port == null || !getFirst || !askSecond) {
-            output.write(HTTP400);
+        if (!getValid) {
+            output.write((HTTP400 + connection + contentType + NEWLINE).getBytes());
+            return false;
+        } else if (!askValid) {
+            output.write((HTTP404 + connection + contentType + NEWLINE).getBytes());
+            return false;
+        } else if (!hostProvided || !portProvided) {
+            output.write((HTTP400 + connection + contentType + NEWLINE).getBytes());
+            return false;
+        } else if (!httpValid) {
+            output.write((HTTP505 + connection + contentType + NEWLINE).getBytes());
+            return false;
         } else {
-            byte[] serverResponse = interact_with_server(output, hostName, port);
-            output_server_response(output, serverResponse);
+            output.write((HTTP200 + connection + contentType).getBytes());
+            return true;
         }
     }
 
-    private static byte[] interact_with_server(OutputStream output, String hostName, Integer port) throws IOException {
+    private static byte[] ask_server() {
+        String hostName = (String) arguments.get("hostname");
+        Integer port = (Integer) arguments.get("port");
         String dataToSend = (String) arguments.get("string");
         Integer limit = (Integer) arguments.get("limit");
         Integer timeout = (Integer) arguments.get("timeout");
         boolean shutdown = arguments.containsKey("shutdown") ? (Boolean) arguments.get("shutdown") : false;
 
-        TCPClient tcpClient = new TCPClient(shutdown, timeout, limit);
         byte[] serverResponse = new byte[0];
 
         try {
-
-            if (dataToSend == null) {
-                serverResponse = tcpClient.askServer(hostName, port);
-            } else {
-                serverResponse = tcpClient.askServer(hostName, port, (dataToSend + "\r\n").getBytes());
-            }
-        } catch (Exception exception) {
-            byte[] HTTP404 = "HTTP/1.1 404 Not Found\r\n\r\n".getBytes();
-            output.write(HTTP404);
+            TCPClient tcpClient = new TCPClient(shutdown, timeout, limit);
+            return dataToSend == null ?
+                    tcpClient.askServer(hostName, port) :
+                    tcpClient.askServer(hostName, port, (dataToSend + NEWLINE).getBytes());
+        } catch (IOException exception) {
+            return serverResponse;
         }
-
-        return serverResponse;
     }
 
-    private static void output_server_response(OutputStream output, byte[] serverResponse) throws IOException {
-        byte[] HTTP200 = "HTTP/1.1 200 OK\r\n\r\n".getBytes();
-        output.write(HTTP200);
-        output.write(serverResponse);
 
-        if(serverResponse.length > 0) {
+    private static void output_response(OutputStream output, byte[] serverResponse) throws IOException {
+        output.write(("Content-Length: " + serverResponse.length + NEWLINE + NEWLINE).getBytes());
+        if (serverResponse.length > 0) {
+            output.write(serverResponse);
+
             char lastCharOfResponse = (char) serverResponse[serverResponse.length - 1];
             if (lastCharOfResponse != '\n') {
-                output.write('\r');
-                output.write('\n');
+                output.write(NEWLINE.getBytes());
             }
         }
     }
