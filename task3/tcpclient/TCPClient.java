704eaf62907b79ab533d1a1318d2480440e2b391
Azmeer K <azmeer.khawaja@outlook.com>
2023-02-24 13:03:43 +0100

    Version of class which was graded as passed.

--- task3/tcpclient/TCPClient.java
+++ task3/tcpclient/TCPClient.java
@@ -9,71 +9,90 @@ public class TCPClient {
     private static Integer timeout_ms;
     private static Integer limit_bytes;
 
-    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+    public TCPClient( boolean shutdown, Integer timeout, Integer limit ) {
         shutdown_boolean = shutdown;
         timeout_ms = timeout == null || timeout < 0 ? 0 : timeout;
         limit_bytes = limit;
     }
 
-    public byte[] askServer(String hostname, int port, byte[] toServerBytes) throws IOException {
-        if (toServerBytes.length == 0) {
-            return askServer(hostname, port);
+    public byte[] askServer( String hostname, int port, byte[] toServerBytes ) throws IOException {
+        if ( toServerBytes.length == 0 ) {
+            return askServer( hostname, port );
         }
 
-        Socket socket = new Socket(hostname, port);
-        write_to_socket(socket, toServerBytes);
-        byte[] bytesFromServer = retrieve_from_socket(socket);
-        socket.close();
+        byte[] bytesFromServer = new byte[ 0 ];
+        try {
+            Socket socket = new Socket( hostname, port );
+
+            write_to_socket( socket, toServerBytes );
+            bytesFromServer = retrieve_from_socket( socket );
+
+            socket.close();
+        }
+        catch ( Exception exception ) {
+            exception.printStackTrace();
+        }
+
         return bytesFromServer;
     }
 
-    public byte[] askServer(String hostname, int port) throws IOException {
-        Socket socket = new Socket(hostname, port);
-        byte[] bytesFromServer = retrieve_from_socket(socket);
-        socket.close();
+    public byte[] askServer( String hostname, int port ) throws IOException {
+        byte[] bytesFromServer = new byte[ 0 ];
+        try {
+            Socket socket = new Socket( hostname, port );
+
+            bytesFromServer = retrieve_from_socket( socket );
+
+            socket.close();
+        }
+        catch ( Exception exception ) {
+            exception.printStackTrace();
+        }
+
         return bytesFromServer;
     }
 
-    private void write_to_socket(Socket socket, byte[] bytesToServer) throws IOException {
+    private void write_to_socket( Socket socket, byte[] bytesToServer ) throws IOException {
         OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(bytesToServer);
-        if (shutdown_boolean) {
+        outputStream.write( bytesToServer );
+        if ( shutdown_boolean ) {
             socket.shutdownOutput();
         }
     }
 
-    private byte[] retrieve_from_socket(Socket socket) throws IOException {
+    private byte[] retrieve_from_socket( Socket socket ) throws IOException {
         ByteArrayOutputStream dynamicBuffer = new ByteArrayOutputStream();
 
-        if (limit_bytes != null && buffer_size < limit_bytes) {
+        if ( limit_bytes != null && buffer_size < limit_bytes ) {
             buffer_size = limit_bytes;
         }
 
-        byte[] buffer = new byte[buffer_size];
+        byte[] buffer = new byte[ buffer_size ];
 
         try {
             InputStream inputStream = socket.getInputStream();
-            socket.setSoTimeout(timeout_ms);
+            socket.setSoTimeout( timeout_ms );
 
             int readBytes;
 
-            if (limit_bytes == null) {
-                while ((readBytes = inputStream.read(buffer)) != -1) {
-                    dynamicBuffer.write(buffer, 0, readBytes);
+            if ( limit_bytes == null ) {
+                while ( ( readBytes = inputStream.read( buffer ) ) != -1 ) {
+                    dynamicBuffer.write( buffer, 0, readBytes );
                 }
             } else {
                 int limit = limit_bytes;
-                while ((readBytes = inputStream.read(buffer, 0, limit)) != -1) {
-                    dynamicBuffer.write(buffer, 0, readBytes);
+                while ( ( readBytes = inputStream.read( buffer, 0, limit ) ) != -1 ) {
+                    dynamicBuffer.write( buffer, 0, readBytes );
 
                     limit -= readBytes;
-                    if (limit == 0) {
+                    if ( limit == 0 ) {
                         break;
                     }
                 }
             }
-        } catch (SocketTimeoutException exception) {
-            System.out.println("Socket Timeout!\n");
+        }
+        catch ( SocketTimeoutException exception ) {
+            System.out.println( "Socket Timeout!\n" );
         }
 
         return dynamicBuffer.toByteArray();
